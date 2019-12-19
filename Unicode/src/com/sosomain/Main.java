c90094bffaf1ed6e55ad7879dcf2a3feb75f9b8a
bh98mex <33382877+bh98mex@users.noreply.github.com>
2019-12-19 23:31:54 +0000

    Add files via upload
    Symbols in Java

---
+++ Unicode/src/com/sosomain/Main.java
@@ -0,0 +1,28 @@
+package com.sosomain;
+
+public class Main {
+
+    public static void main(String[] args) {
+    char MyChar = '\u00a9';
+        char MySecondChar = '\u03E2';
+        System.out.println("Unicode output wsa: " + MyChar);
+        System.out.println("Unicode output was : " + MySecondChar);
+        //make star shape
+        System.out.println  ("*    *    *");
+        System.out.println  (" *   *   *");
+        System.out.println  ("  *  *  *");
+        System.out.println  (" *   *   *");
+        System.out.println  ("*    *    *");
+        System.out.println ("*     *     *");
+        double MyDoubleNumber = 500d;
+        double MySecondDouble = MyDoubleNumber / 2;
+        System.out.println("My Second Double = " + MySecondDouble);
+        boolean MyBoolean = true;
+        boolean isMAle = false;
+        char mysecondchar ='\u03B8';
+        System.out.println("unicode print was : "  + mysecondchar);
+        //1.Find code for the registered symbol on the same line as the copyright symbol
+        //2.Create a variable type char and assign it the unicode value for the same simbol.
+        //3.Dispaly it on the screen.
+    }
+}
