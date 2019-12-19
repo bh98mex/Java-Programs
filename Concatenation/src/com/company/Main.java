b725fcd5f12d5af523c6d4ab90508ad4aac90440
bh98mex <33382877+bh98mex@users.noreply.github.com>
2019-12-19 23:59:08 +0000

    Add files via upload
    Concatenation in Java

---
+++ Concatenation/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner; //importing Scanner class
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter string for c");
+        String c=sc.nextLine();
+        System.out.println("string for c is: " +c);
+        System.out.println("enter string for d");
+        String d=sc.nextLine();
+        System.out.println("d is: " + d);
+    }
+}
