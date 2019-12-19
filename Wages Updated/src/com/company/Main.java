228c6e34c0ad1203425b957c694bd1dc7d77694e
bh98mex <33382877+bh98mex@users.noreply.github.com>
2019-12-19 23:44:23 +0000

    Add files via upload
    Calculate the wage and hours based on keyboard input

---
+++ Wages Updated/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner keyboard = new Scanner (System.in);
+        System.out.println("\n");
+        double HoursPerDay, HoursToday, HoursPerWeek;
+        double final1 = HoursPerDay = 8;
+        HoursPerWeek = HoursPerDay * 5;
+        System.out.println("\n");
+        System.out.println("Total Hours per Day = 8");
+        double pay = 7.20;
+        System.out.println("Total Pay per Hour is " + pay);
+        System.out.println("Enter hours today...");
+        HoursToday = keyboard.nextDouble();
+        System.out.println("Total Hours per week..." + HoursPerWeek);
+        double finalPay = HoursPerDay * 7.20;
+        System.out.println("Total Pay per Week " + finalPay);
+
+
+
+
+
+    }
+}
