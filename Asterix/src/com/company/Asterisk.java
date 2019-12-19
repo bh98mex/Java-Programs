902f9cbbd6f662bf287211a5485a2f2249439d6b
bh98mex <33382877+bh98mex@users.noreply.github.com>
2019-12-19 23:17:16 +0000

    Add files via upload
    Simple hard coded program to display a shape in Java

---
+++ Asterix/src/com/company/Asterisk.java
@@ -0,0 +1,24 @@
+
+package com.company;
+/*
+Project:         Assessment1;
+Name:            Leo Groza;
+Student Number:  B00727232;
+Due Date:        26/10/2016.
+Comment:    Use an appropriate combination of print and println statements to achieve the contents of the star.
+ */
+public class Asterisk {
+//Print out a star shape.
+    public static void main(String[] args) {
+        //	A series of asterisks print statements
+        System.out.print    ("    *   \n");
+        System.out.println  (" *  *  *  ");
+        System.out.println  ("  * * *   ");
+        System.out.println  ("   ***    ");
+        System.out.println  ("**********");
+        System.out.println  ("   ***    ");
+        System.out.println  ("  * * *   ");
+        System.out.println  (" *  *  *  ");
+        System.out.print    ("    *   \n");
+    }//main method
+}//class
