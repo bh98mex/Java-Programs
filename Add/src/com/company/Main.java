718cf2b82bafcdb85707c576a28468a4e1b3422b
bh98mex <33382877+bh98mex@users.noreply.github.com>
2019-12-20 00:49:45 +0000

    Add files via upload
    Add numbers in Java

---
+++ Add/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+import java.util.Scanner;
+
+ class Main {
+
+    public static void main(String[] args) {
+
+        //write your code here
+        int result = 1+2;
+        int myResult;
+        System.out.println("1+2 = " + result);
+
+        int previousResulst = result+5;
+        System.out.println("Previous result is " + previousResulst);
+
+        result = myResult = 5+7;
+        System.out.println(result);
+
+        myResult= result -5;
+        System.out.println(myResult);
+
+        myResult --;
+
+        System.out.println("My result is now " +myResult);
+
+        myResult++;
+
+        System.out.println("My result is now " +myResult);
+
+        myResult +=2;
+
+        System.out.println("My result is now " +myResult);
+
+        myResult += -5;
+
+        System.out.println("My result is now " +myResult);
+
+        myResult *=10;
+        System.out.println("My result is now " +myResult);
+
+        boolean rezultat;
+        rezultat = false;
+if (rezultat==false)
+        System.out.println("You have done well");
+else;
+        System.out.println("GREAT");
+
+
+    }
+}
+
+
+
