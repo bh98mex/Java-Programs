19a3edf2b9d397c2d035330b7d80d5f9c958edb2
bh98mex <33382877+bh98mex@users.noreply.github.com>
2019-12-20 00:53:40 +0000

    Add files via upload
    Showing a message on the screen with JPane

---
+++ ScreenMessage/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Showing a message on the screen");
+
+        JOptionPane.showMessageDialog(null, "welcome to my program","Mesaj de intampinare", JOptionPane.INFORMATION_MESSAGE);
+
+
+	// write your code here
+    }
+}
