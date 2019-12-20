8bd9a94608a64f4aa50c654461e6d1b428424aaf
bh98mex <33382877+bh98mex@users.noreply.github.com>
2019-12-20 00:21:39 +0000

    Add files via upload
    Simple hard coded address

---
+++ Accommodation1/src/com/company/Accommodation1.java
@@ -0,0 +1,48 @@
+/*
+Project name: TestTaskB
+Created by: Leo Groza
+Student number: B00727232
+ */
+
+package com.company;
+import java.util.Scanner;
+
+//create your class Accommodation1
+//create your program
+
+class  Accommodation1 {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+
+
+     String String1 = "Chalkhill Road";
+        String String2 = "Wembley";
+        String String3 = "Value of the house is";
+                  double value1 = 900000;
+        int int1 = 31;
+
+        char mychar = 'y';
+        char mychar1 = 'n';
+
+        System.out.println("I live in" + String2);
+        System.out.println("The street name is " + String1);
+        System.out.println("Number is " + int1);
+        System.out.println("Total value of the house is " + value1);
+
+        System.out.println("Would you like to see the info again: y = yes, n = no");
+         //mychar = keyboard.hasNext();
+        //followed by a switch case
+
+
+
+
+
+
+
+        // write your code here
+
+	// write your code here
+    }
+}
